0a8451225369f3dfafc3833fc1a84dc566f7912c
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2020-07-02 03:34:39 +0530

    Update binary_search.cpp

--- StandardAlgorithms/binary_search.cpp
+++ StandardAlgorithms/binary_search.cpp
@@ -2,7 +2,7 @@
 int binary_search(vector<int>& a, int l, int r, int k){
     if(l>r) return -1;
     int mid = l + (r-l)/2;
-    if(a[mid] == k) return mid;
+    if(k == a[mid]) return mid;
     else if(k > a[mid]) return binary_search(a,mid+1,r,k);
     return binary_search(a,l,mid-1,k);
 }
@@ -11,7 +11,7 @@ int binary_search(vector<int>& a, int l, int r, int k){
 int binary_search(vector<int>& a, int l, int r, int k){
     while(l<=r){
         int mid = l + (r-l)/2;
-        if(a[mid]==k) return mid;
+        if(k == a[mid]) return mid;
         else if(k > a[mid]) l = mid+1;
         else r = mid-1;
     }
