dd4f449b1ed1d6954b4d74bd7e0c667679423258
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2020-07-02 04:09:16 +0530

    Update binary_search.cpp

--- StandardAlgorithms/binary_search.cpp
+++ StandardAlgorithms/binary_search.cpp
@@ -17,3 +17,85 @@ int binary_search(vector<int>& a, int l, int r, int k){
     }
     return -1;
 }
+
+
+// AGGRCOW - Aggressive cows
+
+// iterative binary search
+int n,c;
+vector<int> a;
+ 
+int f(int x){
+    int cows=1, lastpos=a[0];
+    for(int i=1;i<n;i++){
+        if(a[i]-lastpos >= x ){
+            cows++;
+            lastpos=a[i];
+        }
+        if(cows>=c)  return 1;
+    }
+    return 0;
+}
+ 
+void solve(){
+    cin>>n>>c;
+    a.resize(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    sort(a.begin(),a.end());
+    int l=0, r=a[n-1]-a[0]+1,mid;
+    while(r-l>1){
+        mid = (l+r)/2;
+        if(f(mid)==0)   r=mid;
+        else l = mid;
+    }
+    cout<<l<<endl;
+}
+
+
+// recursive binary search
+
+bool isPossible(vector<ll>& a, ll c, ll d){
+    ll n = a.size();
+    ll nxt = 0;
+    int cows = 0;
+    for(ll i=0;i<n;i++){
+        if(cows >= c) return true;
+
+        if(a[i] >= nxt) {
+            cows++;
+            nxt = a[i] + d;
+        }
+    }
+    if(cows >= c) return true;
+    return false;
+}
+
+// return max distance b/w cows possible
+ll fit_cows(vector<ll>& a, ll l, ll r, ll c){
+    if(l>r) return 0;
+    ll mid = (l+r)/2;
+    if(isPossible(a,c,mid)){
+        // bug1(mid);
+        return max( mid, fit_cows(a,mid+1,r,c) );
+    }
+    else{
+        return fit_cows(a,l,mid-1,c);
+    }
+
+}
+
+void solve(){
+    ll n,c;
+    cin>>n>>c;
+    vector<ll> a(n);
+    for(ll i=0;i<n;i++) cin>>a[i];
+    sort(a.begin(), a.end());
+    // let d be distance b/w cows now do a Bsearch on d
+    ll l=0, r=1e9 + 5;
+    cout<< fit_cows(a,l,r,c) <<"\n";
+    return;
+}
+
+
