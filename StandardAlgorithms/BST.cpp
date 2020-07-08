436af6c1c78cbbd14e816a1678de1fe68eccc364
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2020-07-09 00:35:46 +0530

    Create BST.cpp

---
+++ StandardAlgorithms/BST.cpp
@@ -0,0 +1,19 @@
+// BST Insertion
+
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* node, int val) {
+        if(node == NULL){
+            TreeNode* newNode = new TreeNode(val);
+            return newNode;
+        }
+        if(val < node->val){
+            node->left = insertIntoBST(node->left, val);
+        }
+        else{
+            node->right = insertIntoBST(node->right, val);
+        }
+        return node;
+    }
+};
+
