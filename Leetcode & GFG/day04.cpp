78d4265f9ef531247c8d0ce6d215480a0e91a165
Amit Dubey <35802447+amitdu6ey@users.noreply.github.com>
2020-07-13 12:14:54 +0530

    Update day04.cpp

--- Leetcode & GFG/day04.cpp
+++ Leetcode & GFG/day04.cpp
@@ -36,3 +36,26 @@ public:
         return {};
     }
 };
+
+
+3. Longest Consequtive elements
+
+// Brute Force
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        sort(nums.begin(), nums.end());
+        int ans = 1, cur = 1;
+        for(int i=1; i<nums.size(); i++) {
+            if(nums[i] == nums[i-1] + 1){
+                cur++;
+                ans = max(ans, cur);
+            }
+            else if(nums[i] != nums[i-1])
+                cur = 1;
+        }
+        return ans;
+    }
+};
